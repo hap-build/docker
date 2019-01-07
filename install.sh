0ed5a84608736a68b4f8d77da2f76b20574594f5
gwoo <gwoohoo@gmail.com>
2019-01-06 16:23:20 -0800

    update docker install

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 set -eu
 export DEBIAN_FRONTEND=noninteractive
 
-VERSION="17.12.0-ce"
+VERSION="18.09.0-ce"
 if [ ! -z "$1" ]; then
-  VERSION=$1
+	VERSION=$1
 fi
 DOCKER_INSTALLED=$(which docker)
 if [ ! -z "$DOCKER_INSTALLED" ]; then
@@ -17,35 +17,21 @@ fi
 
 echo "Installing packages"
 apt-get -y -qq update
-apt-get -y -q remove  docker docker-engine docker.io
+apt-get -y -q remove docker docker-engine docker.io containerd runc
 apt-get -y -q purge "docker*" "docker-engine*" "docker.io*" "lxc-docker*"
 apt-get install -y -q apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 apt-get -y -qq update
 apt-get install -y -q docker-ce=${VERSION}
 
-FILE=/etc/default/grub
-VAR="GRUB_CMDLINE_LINUX"
-VALUE="cgroup_enable=memory swapaccount=1"
-if ! grep -q "$VAR" $FILE; then
-	echo "Adding $VAR"
-	echo "$VAR=\"$VALUE\"" >> $FILE
-else
-	echo "Updating $VAR with $VALUE"
-	perl -pi -e "s|^$VAR=.*\n||" $FILE
-	echo "$VAR=\"$VALUE\"" >> $FILE
-fi
-sudo update-grub2
-ufw disable
-
 if id ${USER} >/dev/null 2>&1; then
-echo "Add ubuntu to docker group"
-sudo usermod -aG docker ${USER}
-sudo usermod -aG root ${USER}
+	echo "Add ubuntu to docker group"
+	sudo usermod -aG docker ${USER}
+	sudo usermod -aG root ${USER}
 fi
 
 echo "Restarting docker"
